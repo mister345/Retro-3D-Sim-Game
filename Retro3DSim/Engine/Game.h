3bab1373d728787f89bd2e58cbae6e1cbad85138
Gregory Weiner <gregory.weiner@gmail.com>
2016-10-27 22:23:52 +0700

    Uploaded base files

---
+++ Retro3DSim/Engine/Game.h
@@ -0,0 +1,57 @@
+/****************************************************************************************** 
+ *	Chili DirectX Framework Version 14.03.22											  *	
+ *	Game.h																				  *
+ *	Copyright 2014 PlanetChili.net <http://www.planetchili.net>							  *
+ *																						  *
+ *	This file is part of The Chili DirectX Framework.									  *
+ *																						  *
+ *	The Chili DirectX Framework is free software: you can redistribute it and/or modify	  *
+ *	it under the terms of the GNU General Public License as published by				  *
+ *	the Free Software Foundation, either version 3 of the License, or					  *
+ *	(at your option) any later version.													  *
+ *																						  *
+ *	The Chili DirectX Framework is distributed in the hope that it will be useful,		  *
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+ *	GNU General Public License for more details.										  *
+ *																						  *
+ *	You should have received a copy of the GNU General Public License					  *
+ *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+ ******************************************************************************************/
+#pragma once
+
+#include "Keyboard.h"
+#include "Mouse.h"
+#include <fstream>
+#include "Graphics.h"
+#include "Polyhedron.h"
+
+class Game
+{
+public:
+	Game( class MainWindow& wnd );
+	Game( const Game& ) = delete;
+	Game& operator=( const Game& ) = delete;
+	void Go();
+    void DoInput();
+private:
+    void ComposeFrame();
+	void UpdateModel();
+
+	/********************************/
+	/*  User Functions              */
+	/********************************/
+private:
+	MainWindow& wnd;
+	Graphics m_gfx;
+
+	/********************************/
+	/*  User Variables              */
+	int m_x = 0;
+	int m_y = 0;
+	std::wofstream log;
+    Polyhedron m_poly;
+    Polyhedron m_poly2;
+
+	/********************************/
+};
